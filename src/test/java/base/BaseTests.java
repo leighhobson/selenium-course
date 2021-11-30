01516a70ab7729e46a6ff67ffef4e08dc215f774
leighhobson <leighhobson@xpertai.com>
2021-11-30 11:50:34 +0100

    QAA-1837 - Chapter 3 - Find all links on a page and print to console.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,11 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
@@ -11,8 +14,13 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().setSize(new Dimension(375, 812));
-        System.out.println(driver.getTitle());
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
         driver.quit();
     }
     public static void main(String args[]){
