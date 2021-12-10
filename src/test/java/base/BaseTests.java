3a749fc3740de2ccca7b9a70efce454df2c62c97
leighhobson <leighhobson@xpertai.com>
2021-12-10 09:07:02 +0100

    QAA-1837 - Chapter 13 - Event Listeners - Changed BaseTests to set up EventFiringWebDriver and register EventReporter, and set up a new EventReporter class in utils directory to print out console information before clicking on a link.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,28 +5,33 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
 
 public class BaseTests {
-    private WebDriver driver;
+//  private WebDriver driver; // commented out to allow EventFiringWebDriver example.
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        // driver.manage().timeouts().pageLoadTimeout(); //page load timeout example (Chapter 9)
-        // driver.manage().timeouts().setScriptTimeout(); //asynchronous scripts timeout example (Chapter 9)
-        // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //implicit wait example (Chapter 9)
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
+    //  driver = new ChromeDriver();
+    //  driver.manage().timeouts().pageLoadTimeout(); //page load timeout example (Chapter 9)
+    //  driver.manage().timeouts().setScriptTimeout(); //asynchronous scripts timeout example (Chapter 9)
+    //  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //implicit wait example (Chapter 9)
         goHome();
 
         homePage = new HomePage(driver);
